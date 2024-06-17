0019a03cc5bdb7cf1b78644af50f4f85c13a2f16
mattiabascelli <166599366+mattiabascelli@users.noreply.github.com>
2024-06-17 18:04:55 +0200

    Add files via upload

---
+++ generatore.js
@@ -0,0 +1,7 @@
+﻿// JavaScript source code
+function fightGenerator() {
+    const MAX = 10;
+    var num = Math.trunc(MAX * Math.random()) + 1;
+    incontro.src = "img/0" + num + ".jpg";
+    nome.innerHTML = "hai estratto la carta " + num;
+}
